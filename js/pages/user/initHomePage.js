eaaaa79a7f8f51f54ed91947052b1861c3889ca6
Pedro Sabino <22402721@aluno.cotemig.com.br>
2026-08-21 08:10:30 -0300

    fix: import errado

--- js/pages/user/initHomePage.js
+++ js/pages/user/initHomePage.js
@@ -1,12 +1,12 @@
 import { preencherPainelPerfil } from "./preencherPerfil.js";
-import { iniciarMonitoramentoSessao } from "../../auth/logic/watchSession.js";
+import { iniciarMonitoramentoSessao } from "../auth/logic/watchSession.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const bruto = sessionStorage.getItem("bion-dados-usuario");
   if (!bruto) {
     // sessionStorage vazio = chegou aqui sem passar pelo afterLogin
     // (ex: digitou a URL direto). Mais seguro mandar pro login.
-    window.location.href = "../../../auth/login.html";
+    window.location.href = "../../auth/login.html";
     return;
   }
 
